909e8ab6b2371b85bef571022704be015a474425
purple-prince <himynameischuk44@gmail.com>
2023-11-11 21:33:42 -0800

    initial app colors

--- Squiggle/ContentView.swift
+++ Squiggle/ContentView.swift
@@ -9,17 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(spacing: 0) {
+            VStack(spacing: 0) {
+                Color.white.ignoresSafeArea()
+                Color.offWhite.ignoresSafeArea()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
-// first commit
+
+extension Color {
+    static let offWhite = Color(red: 246/255, green: 246/255, blue: 246/255)
+    static let appYellow = Color(red: 1, green: 203/255, blue: 116/255)
+    static let appDark = Color(red: 17/255, green: 17/255, blue: 17/255)
+    static let appDarkSecondary = Color(red: 47/255, green: 47/255, blue: 47/255)
+}
+
+
