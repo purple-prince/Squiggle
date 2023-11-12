0e291bf62a6319214194d9b819c95adf48dc1a4d
purple-prince <himynameischuk44@gmail.com>
2023-11-11 20:42:15 -0800

    Initial Commit

---
+++ Squiggle/SquiggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquiggleApp.swift
+//  Squiggle
+//
+//  Created by Charlie Reeder on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SquiggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
