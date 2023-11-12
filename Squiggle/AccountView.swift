5044e9826745f9eee081f4ca034f50b3ed61b3f7
purple-prince <himynameischuk44@gmail.com>
2023-11-11 21:59:08 -0800

    navigation tabs setup

---
+++ Squiggle/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  Squiggle
+//
+//  Created by Charlie Reeder on 11/11/23.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
---
+++ Squiggle/Files/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Squiggle
+//
+//  Created by Charlie Reeder on 11/11/23.
+//
+
+import Foundation
